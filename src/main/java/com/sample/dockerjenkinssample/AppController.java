bcedbd74e38c51d6d9b194e75c0b3dfa7c01d2d3
ByeongGil-Jung <jbkcose@gmail.com>
2019-07-20 17:09:34 +0900

    [19-07-20] Version changed to 0.0.2 - Version changed to 0.0.2

--- src/main/java/com/sample/dockerjenkinssample/AppController.java
+++ src/main/java/com/sample/dockerjenkinssample/AppController.java
@@ -9,7 +9,7 @@ public class AppController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.0.1");
+        model.addAttribute("version", "0.0.2");
 
         return "index";
     }
