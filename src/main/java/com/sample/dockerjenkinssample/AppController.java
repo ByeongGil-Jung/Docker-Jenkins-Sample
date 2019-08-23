1f851ea107fce0ef6de883d85558eeefa1c4d746
ByeongGil-Jung <jbkcose@gmail.com>
2019-08-23 19:14:58 +0900

    [19-08-23] Branch Test - develop - Branch Test - develop

--- src/main/java/com/sample/dockerjenkinssample/AppController.java
+++ src/main/java/com/sample/dockerjenkinssample/AppController.java
@@ -9,7 +9,7 @@ public class AppController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.0.16");
+        model.addAttribute("version", "0.0.17");
 
         return "index";
     }
